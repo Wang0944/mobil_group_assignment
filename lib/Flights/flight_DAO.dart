930d486854f9cfc98b749a653156096a8f42afbf
CC-Wing <se.wing.huang@gmail.com>
2024-08-04 15:13:40 -0400

    Flight DAO creation. May need to revisit.

--- lib/Flights/flight_DAO.dart
+++ lib/Flights/flight_DAO.dart
@@ -1,1 +1,19 @@
-// TODO Implement this library.
+import 'package:floor/floor.dart';
+import 'flights.dart';
+
+@dao
+abstract class flight_DAO{
+
+  @insert
+  Future<void> insertFlight(flights trip);
+
+  @delete
+  Future<void> deleteFlight(flights trip);
+
+  @update
+  Future<void> updateFlight(flights trip);
+
+  @Query('SELECT * from flights')
+  Future< List< flights > > getFlights();
+
+}
